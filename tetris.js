5051402084216e4a4380ba273500b658e6e75c74
1eandro <leandro.f.smart@gmail.com>
2017-12-17 17:11:29 -0200

    adding piece shapes

--- tetris.js
+++ tetris.js
@@ -4,11 +4,6 @@ const context = canvas.getContext("2d")
 
 context.scale(20, 20)
 
-const matrix = [
-    [0, 0, 0],
-    [1, 1, 1],
-    [0, 1, 0],
-]
 
 function collide(arena, player){
 	const m = player.matrix
@@ -32,6 +27,60 @@ function createMatrix(w,h){
 		matrix.push(new Array(w).fill(0))
 	}
 	return matrix
+}
+
+function createPiece(type){
+
+	if(type ==='T'){
+		return 	[
+				    [0, 0, 0],
+				    [1, 1, 1],
+				    [0, 1, 0],
+				]
+	}else if(type === 'O'){
+		return 	[
+					[1,1],
+					[1,1]
+				]
+	}else if(type === 'L'){
+		return 	[	
+					[0, 1, 0],
+				    [0, 1, 0],
+				    [0, 1, 1],
+				] 
+
+	}else if(type === 'J'){
+		return 	[	
+					[0, 1, 0],
+				    [0, 1, 0],
+				    [1, 1, 0],
+				] 
+
+	}else if(type === 'I'){
+		return 	[	
+					[0, 1, 0, 0],
+				    [0, 1, 0, 0],
+				    [0, 1, 0, 0],
+				    [0, 1, 0, 0],
+				] 
+
+	}else if(type === 'S'){
+		return 	[	
+					[0, 1, 1],
+				    [1, 1, 0],
+				    [0, 0, 0],
+				] 
+
+	}else if(type === 'Z'){
+		return 	[	
+					[1, 1, 0],
+				    [0, 1, 1],
+				    [0, 0, 0],
+				] 
+
+	}
+
+
 }
 
 
@@ -64,7 +113,7 @@ const arena = createMatrix(12,20)
 
 const player = {
     pos: { x: 5, y: 5 },
-    matrix: matrix
+    matrix: createPiece("T")
 }
 
 
